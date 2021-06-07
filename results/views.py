f626ff365c53a2e24e36b7b7f2ef89999f68f318
Basil <vogurt007@gmail.com>
2021-06-07 11:28:45 +0300

    st 1.0

--- results/views.py
+++ results/views.py
@@ -17,19 +17,31 @@ def index(request):
         "http://localhost:8000/api/v1/all/",
         data=params,
     ).json()
-    
 
     return render(
          request,
          "index.html",
-         {
-             "month_plan": result.get("month_plan"),
-             "next_month_plan": result.get("next_month_plan"),
-             "week_plan": result.get("week_plan"),
-             "next_week_plan": result.get("next_week_plan"),
-             "month_target": result.get("month_target"),
-             "next_month_target": result.get("next_month_target"),
-             "week_target": result.get("week_target"),
-             "next_week_target": result.get("next_week_target"),
-         }
-     )
+        {
+            "month_plan": result.get("month_plan"),
+            "next_month_plan": result.get("next_month_plan"),
+            "week_plan": result.get("week_plan"),
+            "next_week_plan": result.get("next_week_plan"),
+            "month_target": result.get("month_target"),
+            "next_month_target": result.get("next_month_target"),
+            "week_target": result.get("week_target"),
+            "next_week_target": result.get("next_week_target"),
+            "values": result.get("values_list"),
+            "principles": result.get("principles"),
+            "quotes": result.get("quotes"),
+            "algorithms_business": result.get("my_algorithms_business"),
+            "algorithms_relationships": result.get(
+                "my_algorithms_relationships"),
+            "two_mounth": result.get("two_mounth"),
+            "six_mounth": result.get("six_mounth"),
+            "one_years": result.get("one_years"),
+            "books": result.get("books"),
+            "films": result.get("films"),
+            "travels": result.get("travels"),
+            "wishes": result.get("wishes"),
+        }
+    )
