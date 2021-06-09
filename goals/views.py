2bd34a3500d3818db057083e0d50c5cd957561a0
Basil <vogurt007@gmail.com>
2021-06-09 11:49:17 +0300

    st 1.2

--- goals/views.py
+++ goals/views.py
@@ -7,7 +7,7 @@ from rest_framework.response import Response
 
 from django.contrib.auth import get_user_model
 from api.serializers import GoalSerializer
-
+import calendar
 from datetime import date, datetime
 import requests
 
@@ -17,7 +17,19 @@ from goals.models import GoalType, MyGoals
 User = get_user_model()
 
 
+def add_months(sourcedate, months):
+    month = sourcedate.month - 1 + months
+    year = sourcedate.year + month // 12
+    month = month % 12 + 1
+    day = min(sourcedate.day, calendar.monthrange(year, month)[1])
+    return datetime.date(year, month, day)
+
+
 def get_goals(user):
+    today = date.today()
+    two_month_date = add_months(today, 2)
+    six_month_date = add_months(today, 6)
+    year_end_date = add_months(today, 12)
     m = datetime.now().month
     f = lambda m: m-12 if m>12 else m
     y = datetime.now().year
@@ -25,41 +37,26 @@ def get_goals(user):
     start_date = date(y, m, 1)
     end_date = date(y, f(m+1), ndays)
 
-    month_start_date = start_date
-    month_end_date = date(y, f(m+1), (date(y, m+2, 1) - date(y, m+1, 1)).days)
-    six_month_start_date = date(y, f(m+2), 1)
-    six_month_end_date = date(
-        y,
-        f(m+6),
-        (date(y, m+2, 1) - date(y, m+1, 1)).days
-    )
-    year_start_date = date(y, f(m+6), 1)
-    year_end_date = date(
-        y,
-        f(m+12),
-        (date(y, m+2, 1) - date(y, m+1, 1)).days
-    )
-    
-    my_goals = user.my_goals.filter(date__range=[start_date, end_date])
+    my_goals = user.mygoals.filter(date__range=[start_date, end_date])
 
     two_mounth, six_mounth, one_years = [], [], []
 
     for my_goal in my_goals:
-        if my_goal.date >= month_start_date and my_goal.date <= month_end_date:
+        if my_goal.date >= today and my_goal.date <= two_month_date:
             two_mounth.append({
                 "id": my_goal.id,
                 "data": my_goal.text,
                 "image": my_goal.image,
                 "user_id": my_goal.author.id,
             })
-        if my_goal.date >= six_month_start_date and my_goal.date <= six_month_end_date:
+        if my_goal.date >= two_month_date and my_goal.date <= six_month_date:
             six_mounth.append({
                 "id": my_goal.id,
                 "data": my_goal.text,
                 "image": my_goal.image,
                 "user_id": my_goal.author.id,
                 })
-        if my_goal.date >= year_start_date and my_goal.date <= year_end_date:
+        if my_goal.date >= six_month_date and my_goal.date <= year_end_date:
             one_years.append({
                 "id": my_goal.id,
                 "data": my_goal.text,
@@ -78,7 +75,7 @@ def get_goals(user):
 
 @api_view(['GET'])
 def goals(request):
-    result = get_results(request.user)
+    result = get_goals(request.user)
 
     return Response(result)
 
